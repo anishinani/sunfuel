283d39abd6d477c7650cebf25fcfd14bce162a99
katznicho <katznicho@gmail.com>
2021-12-09 17:58:33 +0300

    end-no-edit

--- views/home.php
+++ views/home.php
@@ -179,9 +179,9 @@
 	<!-- ./wrapper -->
 
 	<!-- jQuery -->
-	<script src="../plugins/jquery/jquery.min.js"></script>
+	<script src="/creditpluswebapp/plugins/jquery/jquery.min.js"></script>
 	<!-- jQuery UI 1.11.4 -->
-	<script src="../plugins/jquery-ui/jquery-ui.min.js"></script>
+	<script src="/creditpluswebapp/plugins/jquery-ui/jquery-ui.min.js"></script>
 	<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
 	<script>
 		$.widget.bridge('uibutton', $.ui.button)
