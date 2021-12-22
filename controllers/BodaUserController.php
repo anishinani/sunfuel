6b3c884fa636c5eab416f74cbd3487fcf5fdb3e9
katznicho <katznicho@gmail.com>
2021-12-22 18:30:00 +0300

    end-no-edit

--- controllers/BodaUserController.php
+++ controllers/BodaUserController.php
@@ -21,7 +21,7 @@ class BodaUser extends DbAccess
         if (strpos($phone, "256") !== false) {
             $phone =  str_replace("256", "0", $phone);
         }
-        die($phone);
+
 
 
         $fuel = $array['fuelStationId'];
