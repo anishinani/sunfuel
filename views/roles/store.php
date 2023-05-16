dd31b4a0e63211b263a6894caf714892a010a6ed
katznicho <katznicho@gmail.com>
2023-05-16 10:58:14 +0300

    end-no-edit

--- views/roles/store.php
+++ views/roles/store.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 
-
 require_once("../../utils/dbaccess.php");
 
 require_once("../../utils/activityLogger.php");
@@ -18,64 +17,77 @@ $activity = new ActivityLogger();
 //unset($_SESSION['errors']);
 
 
-if (isset($_POST['addRole'])) {
 
-    $_SESSION['errors'] = array();
+try {
+    if (isset($_POST['addRole'])) {
 
-
-    // var_dump($_POST['permissions']);
-    // die("here");
-
-
-
-    //check errors and clean o
-    foreach ($_POST as $key => $value) {
-        if ($key == 'addRole' || $key == "permissions" || $key ==  "modules") {
-            continue;
-        } else {
-            if ($helpers->checkEmptyFields($value) != NULL) {
-                array_push($_SESSION['errors'],   $key . " " . $helpers->checkEmptyFields($value));
+        $_SESSION['errors'] = array();
+    
+    
+        // var_dump($_POST);
+        // die("here");
+    
+    
+    
+        //check errors and clean o
+        foreach ($_POST as $key => $value) {
+            if ($key == 'addRole' || $key == "permissions" || $key ==  "modules") {
+                continue;
             } else {
-                //clean user input 
-                $dbAccess->clean($value);
+                if ($helpers->checkEmptyFields($value) != NULL) {
+                    array_push($_SESSION['errors'],   $key . " " . $helpers->checkEmptyFields($value));
+                } else {
+                    //clean user input 
+                    $dbAccess->clean($value);
+                }
             }
         }
-    }
-    //check errors and clean
-
-    if ($_POST['permissions'] == NULL) {
-        array_push($_SESSION['errors'],   "choose at least one permission");
-    }
-
-    //die("here");
-
-
-
-    //check session array
-    if (count($_SESSION['errors']) > 0) {
-
-        header("Location:create.php");
-    }
-    //check session array
-    else {
-        unset($_SESSION['errors']);
-        if ($roles->store($_POST)) {
-            $activity->logActivity(
-                $_SESSION['user'],
-                "Created Role",
-                "role created sucessfully",
-                $_SESSION['email'],
-                $_SESSION['gender']
-            );
-
-            //redirect
-            $_SESSION['success'] = "role Added Successfully";
-            header("Location:index.php");
-            //redirect
-        } else {
-            die("Oops there was an error");
+        //check errors and clean
+    
+        if ($_POST['permissions'] == NULL) {
+            array_push($_SESSION['errors'],   "choose at least one permission");
         }
+    
+        //die("here");
+    
+    
+    
+        //check session array
+        if (count($_SESSION['errors']) > 0) {
+    
+            header("Location:create.php");
+        }
+        //check session array
+        else {
+            unset($_SESSION['errors']);
+            if ($roles->store($_POST)) {
+
+                $activity->logActivity(
+                    $_SESSION['user'],
+                    "Created Role",
+                    "role created sucessfully",
+                    $_SESSION['email'],
+                    $_SESSION['auth']
+                );
+    
+                //redirect
+                $_SESSION['success'] = "role Added Successfully";
+                header("Location:index.php");
+                //redirect
+            } else {
+                die("Oops there was an error");
+            }
+        }
+    } else {
+        die("not set");
     }
-} else {
-    die("not set");
+} catch (\Throwable $th) {
+    //throw $th;
+    die($th->getMessage());
 }
+
+
+
+
+
+
