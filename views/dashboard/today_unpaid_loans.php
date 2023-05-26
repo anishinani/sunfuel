fa02e157a0ccd4b16fee13ed5d8efcd37aef0ef4
katznicho <katznicho@gmail.com>
2023-05-26 23:26:51 +0300

    updatd the dashboard

--- views/dashboard/today_unpaid_loans.php
+++ views/dashboard/today_unpaid_loans.php
@@ -20,7 +20,7 @@ startContent();
 //time is in this format 2020-08-20 00:00:00,2023-05-24 07:07:41
 try {
     //code...
-    $sql = "SELECT bodauser.bodaUserName , bodauser.bodaUserPhoneNumber, bodauser.stageId, loan.loanAmount , loan.loanInterest, loan.created_at, loan.updated_at  FROM bodauser INNER JOIN loan ON bodauser.bodaUserPhoneNumber = loan.boadUserId WHERE  loan.status=0 AND DATE(loan.created_at) = CURDATE()";
+    $sql = "SELECT bodauser.bodaUserName , bodauser.bodaUserPhoneNumber, bodauser.stageId, loan.loanAmount , loan.loanInterest, loan.created_at, loan.updated_at  FROM bodauser INNER JOIN loan ON bodauser.bodaUserPhoneNumber = loan.boadUserId WHERE  loan.status=1 AND DATE(loan.created_at) = CURDATE()";
 
 $details = $dbAccess->selectQuery($sql);
 
